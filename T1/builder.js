75b1c8c8604e87783b05f016c70291921e49ac0c
Vinícius Latini <viniciuslatini2001@gmail.com>
2024-11-21 20:48:44 -0300

    feat: add grid plan

---
+++ T1/builder.js
@@ -0,0 +1,65 @@
+import * as THREE from 'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {
+  initRenderer,
+  initCamera,
+  initDefaultBasicLight,
+  setDefaultMaterial,
+  InfoBox,
+  onWindowResize
+} from "../libs/util/util.js";
+
+let scene, renderer, camera, material, light, orbit;; // Initial variables
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
+camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+material = setDefaultMaterial(); // create a basic material
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
+
+// Listen window size changes
+window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
+
+// Show axes (parameter is size of each axis)
+let axesHelper = new THREE.AxesHelper(12);
+scene.add(axesHelper);
+
+const planeMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+  
+  })
+);
+planeMesh.rotateX(-Math.PI / 2);
+scene.add(planeMesh);
+
+const grid = new THREE.GridHelper(10, 10);
+scene.add(grid);
+
+const highlightMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(1, 1),
+  new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    transparent: true
+  })
+);
+highlightMesh.rotateX(-Math.PI / 2);
+highlightMesh.position.set(0.5, 0, 0.5);
+scene.add(highlightMesh);
+
+// Use this to show information onscreen
+let controls = new InfoBox();
+controls.add("Basic Scene");
+controls.addParagraph();
+controls.add("Use mouse to interact:");
+controls.add("* Left button to rotate");
+controls.add("* Right button to translate (pan)");
+controls.add("* Scroll to zoom in/out.");
+controls.show();
+
+render();
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
