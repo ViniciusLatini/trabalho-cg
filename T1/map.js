48cde330ce0dc4a2992e2b541bc40195b6a86ccd
Viniciuslatini <vinicius.latini@estudante.ufjf.br>
2025-02-02 14:49:15 -0300

    feat: add trees & update procedural map

--- T1/map.js
+++ T1/map.js
@@ -2,12 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
 import {
   initRenderer,
-  initCamera,
   initDefaultBasicLight,
-  setDefaultMaterial,
-  InfoBox,
   onWindowResize,
-  createGroundPlaneXZ
 } from "../libs/util/util.js";
 import { SimplexNoise } from '../build/jsm/math/SimplexNoise.js';
 import Stats from '../build/jsm/libs/stats.module.js';
@@ -16,13 +12,18 @@ import GUI from '../libs/util/dat.gui.module.js';
 let scene, renderer, camera, material, light, orbit;; // Initial variables
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer();    // Init a basic renderer
-camera = initCamera(new THREE.Vector3(0, 50, 50)); // Init camera in this position
-material = setDefaultMaterial(); // create a basic material
+camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 9, 0);
 light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
 orbit = new OrbitControls(camera, renderer.domElement); // Enable mouse rotation, pan, zoom etc.
 
+const mapSize = 100
+
 let fogFar = 100
-scene.fog = new THREE.Fog(0xaaaaaa, 1, 100);
+scene.fog = new THREE.Fog(0xaaaaaa, 1, 96);
+scene.background = new THREE.Color(0xaaaaaa);
+
+const heightMatrix = Array(mapSize * 2).fill().map(() => Array(mapSize * 2).fill(0));
 
 const stats = new Stats()
 document.getElementById("webgl-output").appendChild(stats.domElement);
@@ -31,26 +32,13 @@ document.getElementById("webgl-output").appendChild(stats.domElement);
 window.addEventListener('resize', function () { onWindowResize(camera, renderer) }, false);
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const materials = {
-  rock: new THREE.MeshPhongMaterial({ color: '#575757' }),
-  soil: new THREE.MeshPhongMaterial({ color: '#4A3424' }),
-  grass: new THREE.MeshPhongMaterial({ color: '#226923' }),
-};
-const simplex = new SimplexNoise();
 
+const grassMaterial = new THREE.MeshLambertMaterial({ color: '#226923' })
 // Criação de instâncias para cada material
-const rockMesh = new THREE.InstancedMesh(boxGeometry, materials.rock, 100000);
-const soilMesh = new THREE.InstancedMesh(boxGeometry, materials.soil, 100000);
-const grassMesh = new THREE.InstancedMesh(boxGeometry, materials.grass, 100000);
-
+const grassMesh = new THREE.InstancedMesh(boxGeometry, grassMaterial, 40000);
 // Contadores de instâncias
-let rockIndex = 0;
-let soilIndex = 0;
 let grassIndex = 0;
-
 //Indicando para GPU que essas informações serão atualizadas com frequência
-rockMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
-soilMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
 grassMesh.instanceMatrix.setUsage(THREE.DynamicDrawUsage);
 
 // Função para definir a instância
@@ -60,43 +48,83 @@ function setInstance(mesh, index, position) {
   mesh.setMatrixAt(index, matrix);
 }
 
-function makeColumn(height, position) {
-  for (let i = 0; i < height; i++) {
-    let mesh, idx;
+function generateProceduralMap() {
+  const simplex = new SimplexNoise();
 
-    if (i < height - 4) {
-      mesh = rockMesh;
-      idx = rockIndex++;
+  for (let i = (mapSize * -1); i < mapSize; i++) {
+    for (let j = (mapSize * -1); j < mapSize; j++) {
+      const noise = Math.pow((simplex.noise(i / 70, j / 70) + 1) / 2, 1.5);
+      const height = Math.round(noise * 20);
+      heightMatrix[i + mapSize][j + mapSize] = height;
+      setInstance(grassMesh, grassIndex++, { x: i, y: height, z: j });
     }
-    else if (i < height - 2) {
-      mesh = soilMesh;
-      idx = soilIndex++;
-    }
-    else {
-      mesh = grassMesh;
-      idx = grassIndex++;
-    }
-    setInstance(mesh, idx, { x: position.x, y: i + 0.5, z: position.z });
+  }
+
+  grassMesh.instanceMatrix.needsUpdate = true;
+  scene.add(grassMesh);
+}
+
+async function loadTree(ref, idx) {
+  const response = await fetch(`./trees/a${idx%3 + 1}.json`);
+  const data = await response.json();
+
+  const group = new THREE.Group();
+
+  data.forEach(({ position, mesh }) => {
+    const color = mesh.materials[0].color;
+    const material = new THREE.MeshLambertMaterial({ color });
+
+    const voxel = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+
+    // Ajuste de posição baseado no referencial
+    voxel.position.set(
+      position.x + ref.x + 0.5,
+      position.y + ref.y,
+      position.z + ref.z + 0.5
+    );
+
+    group.add(voxel);
+  });
+
+  scene.add(group);
+}
+
+function renderTrees() {
+  for (let idx = 0; idx < 20; idx++) {
+    const i = Math.floor(Math.random() * mapSize * 2) - mapSize;
+    const j = Math.floor(Math.random() * mapSize * 2) - mapSize;
+    const positionRef = { x: i, y: heightMatrix[i + mapSize][j + mapSize] - 0.5, z: j };
+    loadTree(positionRef, idx);
   }
 }
 
-for (let i = -50; i <= 50; i++) {
-  for (let j = -50; j <= 50; j++) {
-    const noise = Math.pow((simplex.noise(i / 30, j / 30) + 1) / 2, 1.5);
-    const height = Math.round(noise * 20);
-    makeColumn(height, { x: i, z: j });
-  }
+let mainLight = new THREE.DirectionalLight(0xffffff, 1);
+mainLight.position.set(10, 20, 10);
+mainLight.castShadow = true;
+mainLight.shadow.mapSize.width = 2048;
+mainLight.shadow.mapSize.height = 2048;
+mainLight.shadow.camera.near = 0.5;
+mainLight.shadow.camera.far = 150;
+
+const shadowHelper = new THREE.CameraHelper(mainLight.shadow.camera);
+scene.add(shadowHelper);
+shadowHelper.visible = false;
+
+let secondaryLight = new THREE.DirectionalLight(0xffffff, 0.3);
+secondaryLight.position.set(-10, -20, -10);
+
+scene.add(mainLight);
+scene.add(secondaryLight);
+
+function updateShadows() {
+  mainLight.shadow.camera.left = -fogFar / 2;
+  mainLight.shadow.camera.right = fogFar / 2;
+  mainLight.shadow.camera.top = fogFar / 2;
+  mainLight.shadow.camera.bottom = -fogFar / 2;
+  mainLight.shadow.camera.updateProjectionMatrix();
 }
 
-// Atualizar os contadores de instância
-rockMesh.instanceMatrix.needsUpdate = true;
-soilMesh.instanceMatrix.needsUpdate = true;
-grassMesh.instanceMatrix.needsUpdate = true;
-
-// Adicionar as instâncias à cena
-scene.add(rockMesh);
-scene.add(soilMesh);
-scene.add(grassMesh);
+updateShadows();
 
 const controls = new function () {
   this.fogFar = fogFar;
@@ -122,9 +150,12 @@ gui.add(controls, 'fogFar', 20, 200)
 // controls.add("* Scroll to zoom in/out.");
 // controls.show();
 
+generateProceduralMap()
+renderTrees()
 render();
 function render() {
   requestAnimationFrame(render);
   renderer.render(scene, camera) // Render scene
+  grassMesh.frustumCulled = true;
   stats.update();
 }
